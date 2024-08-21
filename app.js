d4a715fc22746af6add668b6cd6fba409e27fe5b
mageskylms <mageskylms@gmail.com>
2024-08-21 16:41:37 -0300

    Inclusão da verificação de DE>ATÉ

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 //ativa ao clicar no botão sortear, chama a função que sorteia o número e muda o texto na tela para mostrar o resultado.
-function sortear(){
+function sortear() {
     //puxa valores dos inputs
-    let quantidade = parseInt(document.getElementById('quantidade').value); 
+    let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    //cria a lista para receber números sorteados
-    let sorteados = [];
-    // variável que vai receber o número sorteado e que será adicionado na lista 'sorteados'
-    let numero;
-    //verifica se o número já foi sorteado
-    for (let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
-        while (sorteados.includes(numero)) {
+    let verificacao = verificaDe();
+    if (verificacao == true) {
+        //cria a lista para receber números sorteados
+        let sorteados = [];
+        // variável que vai receber o número sorteado e que será adicionado na lista 'sorteados'
+        let numero;
+        //verifica se o número já foi sorteado
+        for (let i = 0; i < quantidade; i++) {
             numero = obterNumeroAleatorio(de, ate);
+            while (sorteados.includes(numero)) {
+                numero = obterNumeroAleatorio(de, ate);
+            }
+            //inclui o número sorteado na lista
+            sorteados.push(numero);
         }
-        //inclui o número sorteado na lista
-        sorteados.push(numero);
+        //puxa o elemento que mostra os números sorteados
+        let resultado = document.getElementById('resultado');
+        //altera texto que mostra os números sorteados
+        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+        //altera status dos botões
+        alterarStatusBotao('btn-reiniciar');
+        alterarStatusBotao('btn-sortear');
+    }   else{
+        reiniciar();
+        reiniciar();
     }
-    //puxa o elemento que mostra os números sorteados
-    let resultado = document.getElementById('resultado');
-    //altera texto que mostra os números sorteados
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
-    //altera status dos botões
-    alterarStatusBotao('btn-reiniciar');
-    alterarStatusBotao('btn-sortear');
 }
 //obtem o número aleatório utilizando o método Math.Floor
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 //reinicia o programa
-function reiniciar(){
+function reiniciar() {
     alterarStatusBotao('btn-reiniciar');
     alterarStatusBotao('btn-sortear');
     limparCampos();
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
 }
 //altera status dos botões
-function alterarStatusBotao(btn){
+function alterarStatusBotao(btn) {
     let botaoSortear = document.getElementById(btn);
     //verifica status do botão
     if (botaoSortear.classList.contains('container__botao')) {
@@ -49,8 +55,19 @@ function alterarStatusBotao(btn){
     }
 }
 //limpa inputs
-function limparCampos(){
+function limparCampos() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
+}
+//Verifica de o valor do input de é maior que o valor do input até
+function verificaDe() {
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    if (de > ate) {
+        alert('"Do número" não pode ser maior que "Até!');
+        return false;
+    } else {
+        return true;
+    }
 }
